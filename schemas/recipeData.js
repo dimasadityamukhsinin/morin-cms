fed4e448a4112fe1e3e05f936d731db8d007d349
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-09-14 10:35:32 +0700

    🔥 remove slug in recipe category

--- schemas/recipeData.js
+++ schemas/recipeData.js
@@ -24,30 +24,6 @@ export default {
           validation: (Rule) => Rule.required(),
         },
       ]
-    },
-    {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
-      description:
-        "Slug is generated from Title, Lower Characters (a-z), Numericals (0-9), dash (-) and must not start with a /, Minimum 2 Characters, eg: 'project-title'",
-      options: {
-        source: 'title.en',
-        maxLength: 96,
-      },
-      validation: (Rule) =>
-        Rule.custom((slug) => {
-          const regex = /^[a-z0-9]{2,}(?:-[a-z0-9]+)*$/
-          if (slug) {
-            if (slug.current.match(regex) !== null) {
-              return true
-            } else {
-              return 'Not a valid slug'
-            }
-          } else {
-            return 'Required'
-          }
-        }),
     }
   ],
   preview: {

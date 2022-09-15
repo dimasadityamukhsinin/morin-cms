b7fb847db8833daa7af005e0319bfdd13b965a67
dimasadityamukhsinin <dimasadityamukhsinin@gmail.com>
2022-09-15 11:57:19 +0700

    💄 update get morin

--- schemas/getMorin.js
+++ schemas/getMorin.js
@@ -4,9 +4,9 @@ export default {
   type: 'document',
   fields: [
     {
-      title: "SEO",
-      name: "seo",
-      type: "object",
+      title: 'SEO',
+      name: 'seo',
+      type: 'object',
       fields: [
         {
           title: 'EN',
@@ -90,7 +90,7 @@ export default {
             },
           ],
         },
-      ]
+      ],
     },
     {
       title: "Shop",
@@ -111,117 +111,185 @@ export default {
       ]
     },
     {
-      title: 'Shop Online',
-      name: 'shopOnline',
+      title: 'Contents',
+      name: 'Shops',
       type: 'array',
       of: [
         {
-          title: 'Online Martketplace',
-          name: 'onlineMarketPlace',
+          title: 'Content',
+          name: 'shop',
           type: 'object',
           fields: [
             {
               title: 'Title',
               name: 'title',
-              type: 'string',
-            },
-            {
-              title: 'Link',
-              name: 'link',
-              type: 'url',
-            },
-            {
-              name: 'image',
-              title: 'Image',
-              type: 'image',
+              type: 'object',
+              validation: (Rule) => Rule.required(),
               fields: [
                 {
-                  title: 'Edit Alt Text',
-                  name: 'alt',
+                  name: 'en',
+                  title: 'EN',
                   type: 'string',
-                  initialValue: 'Morin',
+                  validation: (Rule) => Rule.required(),
+                },
+                {
+                  name: 'id',
+                  title: 'ID',
+                  type: 'string',
+                  validation: (Rule) => Rule.required(),
                 },
               ],
             },
-          ],
-        },
-      ],
-    },
-    {
-      title: 'Shop Offline',
-      name: 'shopOffline',
-      type: 'object',
-      fields: [
-        {
-          title: 'Major Distributors',
-          name: 'majorDistributors',
-          type: 'array',
-          of: [
             {
-              title: 'Distributor',
-              name: 'distributor',
-              type: 'object',
-              fields: [
+              title: 'Contents',
+              name: 'contents',
+              type: 'array',
+              of: [
                 {
-                  title: 'Title',
-                  name: 'title',
-                  type: 'string',
-                },
-                {
-                  title: 'Link',
-                  name: 'link',
-                  type: 'url',
-                },
-                {
-                  name: 'image',
-                  title: 'Image',
-                  type: 'image',
+                  title: 'Content',
+                  name: 'content',
+                  type: 'object',
                   fields: [
                     {
-                      title: 'Edit Alt Text',
-                      name: 'alt',
+                      title: 'Title',
+                      name: 'title',
+                      type: 'object',
+                      validation: (Rule) => Rule.required(),
+                      fields: [
+                        {
+                          name: 'en',
+                          title: 'EN',
+                          type: 'string',
+                          validation: (Rule) => Rule.required(),
+                        },
+                        {
+                          name: 'id',
+                          title: 'ID',
+                          type: 'string',
+                          validation: (Rule) => Rule.required(),
+                        },
+                      ],
+                    },
+                    {
+                      title: 'Option',
+                      name: 'option',
                       type: 'string',
-                      initialValue: 'Morin',
+                      validation: (Rule) => Rule.required(),
+                      options: {
+                        list: [
+                          { title: 'Link', value: 'link' },
+                          { title: 'Text', value: 'text' },
+                        ],
+                        layout: 'radio',
+                      },
+                      initialValue: 'link',
+                    },
+                    {
+                      title: 'Links',
+                      name: 'links',
+                      type: 'array',
+                      hidden: ({ parent }) => !(parent?.option === 'link'),
+                      of: [
+                        {
+                          title: 'Link',
+                          name: 'link',
+                          type: 'object',
+                          validation: (Rule) => Rule.required(),
+                          fields: [
+                            {
+                              title: 'Title',
+                              name: 'title',
+                              type: 'string',
+                              validation: (Rule) => Rule.required(),
+                            },
+                            {
+                              name: 'image',
+                              title: 'Image',
+                              type: 'image',
+                              validation: (Rule) => Rule.required(),
+                              fields: [
+                                {
+                                  title: 'Edit Alt Text',
+                                  name: 'alt',
+                                  type: 'string',
+                                  initialValue: 'Morin',
+                                },
+                              ],
+                            },
+                            {
+                              title: 'URL',
+                              name: 'url',
+                              type: 'url',
+                            },
+                          ],
+                          preview: {
+                            select: {
+                              title: 'title',
+                            },
+                          },
+                        },
+                      ],
+                    },        
+                    {
+                      title: 'Texts',
+                      name: 'texts',
+                      type: 'array',
+                      hidden: ({ parent }) => !(parent?.option === 'text'),
+                      of: [
+                        {
+                          title: 'Text',
+                          name: 'getText',
+                          type: 'object',
+                          fields: [
+                            {
+                              title: "Title",
+                              name: "title",
+                              type: "object",
+                              fields: [
+                                {
+                                  title: 'EN',
+                                  name: 'en',
+                                  type: 'string',
+                                  validation: (Rule) => Rule.required(),
+                                },
+                                {
+                                  title: 'ID',
+                                  name: 'id',
+                                  type: 'string',
+                                  validation: (Rule) => Rule.required(),
+                                },
+                              ]
+                            },
+                            {
+                              title: 'Description',
+                              name: 'description',
+                              type: 'editorBasic',
+                              validation: (Rule) => Rule.required(),
+                            },
+                          ],
+                          preview: {
+                            select: {
+                              title: 'title.en',
+                            },
+                          },
+                        },
+                      ],
                     },
                   ],
+                  preview: {
+                    select: {
+                      title: 'title.en',
+                    },
+                  },
                 },
               ],
             },
           ],
-        },
-        {
-          title: 'Retailers',
-          name: 'retailers',
-          type: 'array',
-          of: [
-            {
-              title: 'Retail',
-              name: 'retail',
-              type: 'object',
-              fields: [
-                {
-                  title: 'Title - EN',
-                  name: 'title_en',
-                  type: 'string',
-                },
-                {
-                  title: 'Title - ID',
-                  name: 'title_id',
-                  type: 'string',
-                },
-                {
-                  title: 'Description',
-                  name: 'description',
-                  type: 'editorBasic',
-                },
-              ],
-              preview: {
-                select: {
-                  title: 'title_en',
-                },
-              },
+          preview: {
+            select: {
+              title: 'title.en',
             },
-          ],
+          },
         },
       ],
     },
@@ -329,10 +397,10 @@ export default {
       validation: (Rule) => Rule.required(),
       options: {
         list: [
-          {title: 'Black', value: 'black'},
-          {title: 'White', value: 'white'}
+          { title: 'Black', value: 'black' },
+          { title: 'White', value: 'white' },
         ],
-        layout: 'radio'
+        layout: 'radio',
       },
       initialValue: 'white',
     },
